c00ab806ed85f9810db5a75824a0206113a53e39
daryaN2 <91070591+daryaN2@users.noreply.github.com>
2021-12-24 13:13:54 +0600

    Update WorkflowParser.h

--- WorkflowParser.h
+++ WorkflowParser.h
@@ -8,17 +8,17 @@
 #include <string>
 #include <list>
 #include <vector>
-#include <istream>
+#include <fstream>
 #include <map>
 #include <utility>
 #include "BlockDescErr.h"
 
 class WorkflowParser {
-    std::string getNum(std::string &line);
-    std::vector<std::string> getArgs(std::string &line);
-    std::string getName(std::string &line);
+    std::string getNum(std::string &line, int& pos1);
+    std::vector<std::string> getArgs(std::string &line, int& pos1, int& pos2, std::vector<std::string>& nameArgs);
+    std::string getName(std::string &line, int& pos1, int& pos2);
 public:
-    std::map<std::string, std::vector<std::string>> getBlocks(std::istream &in);
+    std::map<std::string, std::vector<std::string>> getBlocks(std::ifstream &in);
 };
 
 
